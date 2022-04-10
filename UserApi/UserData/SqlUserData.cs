403fb318abf7652498b7c0e906369aef6ff24351
piyx <piyx.abhishek@gmail.com>
2022-04-10 22:33:27 +0530

    Add UserData Repo

---
+++ UserApi/UserData/SqlUserData.cs
@@ -0,0 +1,39 @@
+﻿using UserApi.Models;
+
+namespace UserApi.UserData
+{
+    public class SqlUserData : IUserData
+    {
+        private UserContext _context;
+        public SqlUserData(UserContext context)
+        {
+            _context = context;
+        }
+        public void AddUser(User user)
+        {   
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(User user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+
+        public User GetUser(int id)
+        {
+            return _context.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
